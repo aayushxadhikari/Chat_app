c5253b74286b341bc3fb6ad81c67b773e00238db
aayushxadhikari <aayushxadhikari@gmail.com>
2024-12-24 21:47:27 +0545

    Login and Register

---
+++ lib/features/authentication/domain/repositories/auth_repository.dart
@@ -0,0 +1,6 @@
+import 'package:flutterproducts/features/authentication/domain/entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Future<UserEntity> login(String email, String password);
+  Future<UserEntity> register(String username, String email, String password);
+}
